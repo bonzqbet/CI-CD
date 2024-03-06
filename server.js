4b3ef3caa2c70abc5b40202c9aa225c7e3f6bd7e
Phanuwat Sutthiwong <playgroundclub_1@hotmail.com>
2024-03-06 10:42:28 +0700

    update

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+var cors = require('cors');
+
+var app = express();
+
+app.use(cors());
+
+app.listen(3500,function(){
+    console.log('Port is open at '+ 3500);
+});
+
+app.all("/", function(req, res) {
+    res.json({ code: 1001, msg: `Welcome to Express`});
+});
+
+module.exports = app;
